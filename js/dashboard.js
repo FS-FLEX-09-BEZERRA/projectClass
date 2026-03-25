965d160323ce8d2f7a76b3b4a7bf8c68875a8b19
nayaracalenzo <queiroz.14@hotmail.com>
2026-03-24 21:53:35 -0300

    localstorag

--- js/dashboard.js
+++ js/dashboard.js
@@ -36,10 +36,53 @@ let alunos = [
   }
 ];
 
-carregarAlunos(alunos)
+
+const alunosLocal = JSON.parse(localStorage.getItem("alunos"))
+
+carregarAlunos(alunosLocal)
 
 
-function carregarAlunos(alunos) {
+function carregarAlunos(alunosFiltrados) {
+  const alunos = JSON.parse(localStorage.getItem("alunos"))
+  if (alunosFiltrados) {
+    tabela.innerHTML = alunosFiltrados.map((aluno, index) => {
+      const rowBg = index % 2 === 0
+        ? "bg-[#0F172A]"
+        : "bg-[#111827]";
+
+      const statusStyle = {
+        "Ativo": "bg-purple-600/20 text-purple-400",
+        "Trancado": "bg-yellow-600/20 text-yellow-400",
+        "Formado": "bg-blue-600/20 text-blue-400"
+      };
+
+      return `
+    <tr key=${index} class="grid grid-cols-6 items-center px-8 py-5 border-b border-purple-900/20 ${rowBg} hover:bg-purple-900/20 transition duration-300">
+      <td class="flex items-center gap-3 font-medium text-white">
+        <div class="w-9 h-9 rounded-full bg-linear-to-br from-purple-600 to-violet-500 
+                    flex items-center justify-center text-xs font-bold">
+          ${aluno.nome.split(" ").map(n => n[0]).join("").slice(0, 2)}
+        </div>
+        ${aluno.nome}
+      </td>
+
+      <td class="text-gray-300">${aluno.matricula}</td>
+      <td class="text-gray-300">${aluno.curso}</td>
+      <td class="text-gray-300">${aluno.idade}</td>
+
+      <td>
+        <span class="px-3 py-1 rounded-full text-xs font-semibold ${statusStyle[aluno.status]}">
+          ${aluno.status}
+        </span>
+      </td>
+
+      <td class="text-left text-gray-500 hover:text-purple-400 cursor-pointer transition">
+        <i class="fa-regular fa-pen-to-square" onclick="handleEdit(${aluno.id})"></i>
+      </td>
+    </tr>
+  `;
+    }).join("");
+  }
   tabela.innerHTML = alunos.map((aluno, index) => {
     const rowBg = index % 2 === 0
       ? "bg-[#0F172A]"
@@ -79,6 +122,8 @@ function carregarAlunos(alunos) {
   }).join("");
 }
 
+let EditingAluno = null
+
 function handleAdd() {
   const nome = document.getElementById("nome").value
   const matricula = document.getElementById("matricula").value
@@ -87,7 +132,7 @@ function handleAdd() {
   const modal = document.getElementById("my_modal_2")
   const status = document.getElementById("status").value
   const titleModal = document.querySelector("#titleModal")
-
+  
   if (EditingAluno) {
     const aluno = alunos.find((a) => a.id === EditingAluno)
 
@@ -100,19 +145,22 @@ function handleAdd() {
     modal.style.display = "none";
     EditingAluno = null
     titleModal.innerHTML = "Adicionar Aluno"
+    const alunosLocal = JSON.parse(localStorage.getItem("alunos"))
+    localStorage.setItem("alunos", JSON.stringify(alunosLocal))
     carregarAlunos()
   } else {
     const novoId = alunos.length + 1
     const novoAluno = { id: novoId, nome, matricula, curso, idade, status: "Ativo" }
     console.log(novoAluno)
-    alunos.push(novoAluno)
+    const alunosLocal = JSON.parse(localStorage.getItem("alunos"))
+    alunosLocal.push(novoAluno)
     modal.style.display = "none";
     console.log(alunos)
+    localStorage.setItem("alunos", JSON.stringify(alunosLocal))
     carregarAlunos()
   }
 }
 
-let EditingAluno = null
 function handleEdit(id) {
   const tabela = document.getElementById('tabela')
   const nome = document.getElementById("nome")
@@ -155,7 +203,7 @@ function handleClose() {
 
 function handleStatus() {
   const status = document.querySelector("#statusFilter").value
-  
+  const alunos = JSON.parse(localStorage.getItem("alunos")) 
   const alunosFiltrados = alunos.filter((a)=> a.status === status)
 
 
